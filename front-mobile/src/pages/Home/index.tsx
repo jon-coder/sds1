2277b32da4c51f8147e1da8e1e24ce6b2f6746e5
Joao Paulo <joaopaulobrasiliano@gmail.com>
2020-09-20 14:56:58 -0300

    Adicionando rotas no React native

--- front-mobile/src/pages/Home/index.tsx
+++ front-mobile/src/pages/Home/index.tsx
@@ -2,37 +2,41 @@ import React from 'react';
 import { FontAwesome5 as Icon} from '@expo/vector-icons';
 import { Text, StyleSheet, View, Image, Alert } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import Header from '../../components/Header';
+import { useNavigation } from '@react-navigation/native';
 
 const Home = () =>{
-    const handleOnPress = () => {
-        
-    }
+  const navigation = useNavigation();
 
-    return (
-        <>
-            <View style={styles.container}>
-                <Image 
-                source={require('../../assets/gamer.png')}
-                style={styles.gamerImage}
-                />
-                <Text style={styles.title} >Vote agora!</Text>
-                <Text style={styles.subTitle}>Nos diga qual é seu jogo favorito</Text>
-            </View>
-            <View style={styles.footer}>
-                    <RectButton style={styles.button} onPress={handleOnPress}>
-                    <Text style={styles.buttonText}>
-                        COLETAR DADOS
-                    </Text>
-                    <View style={styles.buttonIcon}>
-                        <Text>
-                            <Icon name="chevron-right" color="#FFF" size={25} />
-                        </Text>
-                    </View>
-                </RectButton>
-            </View>
-        </>
-        
-    )
+  const handleOnPress = () => {
+    navigation.navigate('CreateRecord');
+  }
+
+  return (
+    <>
+      <Header />
+      <View style={styles.container}>
+          <Image 
+            source={require('../../assets/gamer.png')}
+            style={styles.gamerImage}
+          />
+          <Text style={styles.title} >Vote agora!</Text>
+          <Text style={styles.subTitle}>Nos diga qual é seu jogo favorito</Text>
+      </View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={handleOnPress}>
+          <Text style={styles.buttonText}>
+            COLETAR DADOS
+          </Text>
+          <View style={styles.buttonIcon}>
+            <Text>
+                <Icon name="chevron-right" color="#FFF" size={25} />
+            </Text>
+          </View>
+        </RectButton>
+      </View>
+    </>        
+  )
 };
 
 const styles = StyleSheet.create({
